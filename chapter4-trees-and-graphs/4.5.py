d7783c818c0e607a258ce2bbe9d51f3323b44ac9
Yuanxu Li <yuanxu.lee@gmail.com>
2015-10-11 01:50:25 -0400

    update is_bst. imporove to O(n)

--- chapter4-trees-and-graphs/4.5.py
+++ chapter4-trees-and-graphs/4.5.py
@@ -5,56 +5,42 @@ class Node:
 		self.left = None
 		self.right = None
 		self.value = value
+	def set_left(self, left):
+		self.left = left
+	def set_right(self, right):
+		self.right = right
 
-	def is_bst(self):
-		"""
-		Recursive solution. For a tree to be BST, the left subtree and the right subtree have to be BST's, and the value of the root
-		must be >= the largest value of the left subtree and <= the smallest value of the right subtree
+	def is_bst(self, min=None, max=None):
+		""" by passing down max and min each level down, we check if each subtree is a bst
 		>>> n0 = Node(8)
 		>>> n1 = Node(6)
 		>>> n2 = Node(9)
 		>>> n3 = Node(3)
 		>>> n4 = Node(7)
-		>>> n5 = Node(7)
+		>>> n5 = Node(8)
 		>>> n6 = Node(10)
 		>>> n7 = Node(2)
-		>>> n0.left = n1
-		>>> n0.right = n2
-		>>> n1.left = n3
-		>>> n1.right = n4
-		>>> n2.left = n5
-		>>> n2.right = n6
-		>>> n3.left = n7
-		>>> n1.is_bst()
-		True
-		>>> n2.is_bst()
-		True
+		>>> n0.set_left(n1)
+		>>> n0.set_right(n2)
+		>>> n1.set_left(n3)
+		>>> n1.set_right(n4)
+		>>> n2.set_left(n5)
+		>>> n2.set_right(n6)
+		>>> n3.set_left(n7)
 		>>> n0.is_bst()
-		False
+		True
 		"""
+		if (min is not None and self.value < min) or (max is not None and self.value > max):
+			return False
 		if self.left is None and self.right is None:
 			return True
 		elif self.left is None:
-			return self.value <= self.right.min_bst() and self.right.is_bst()
+			return self.right.is_bst(self.value, max)
 		elif self.right is None:
-			return self.value >= self.left.max_bst() and self.left.is_bst()
+			return self.left.is_bst(min, self.value)
 		else:
-			return self.value <= self.right.min_bst()\
-				and self.value >= self.left.max_bst()\
-				and self.left.is_bst()\
-				and self.right.is_bst()
+			return self.left.is_bst(min, self.value) and self.right.is_bst(self.value, max)
 
-	def min_bst(self):
-		if self.left:
-			return self.left.min_bst()
-		else:
-			return self.value
-
-	def max_bst(self):
-		if self.right:
-			return self.right.max_bst()
-		else:
-			return self.value
 
 if __name__ == "__main__":
 	import doctest
