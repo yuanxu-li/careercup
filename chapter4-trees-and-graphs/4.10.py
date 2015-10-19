30597dad8b6f4f7c04d6a13a1dcb5f6d8c341341
Yuanxu Li <yuanxu.lee@gmail.com>
2015-10-19 15:05:13 -0400

    check subtree updated. time complexity increased but space complexity reduced for these are two large trees

--- chapter4-trees-and-graphs/4.10.py
+++ chapter4-trees-and-graphs/4.10.py
@@ -61,6 +61,8 @@ class Node:
 
 	def is_subtree_of(self, node):
 		""" if a tree has the same preorder traversal as a part of another tree, it is a subtree of the second tree
+		time complexity: O(n+m), which is the time to construct the preorders but occupies too much space
+		for the two trees are too large
 		>>> n0 = Node(8)
 		>>> n1 = Node(6)
 		>>> n2 = Node(10)
@@ -87,6 +89,71 @@ class Node:
 		False
 		"""
 		return ''.join(map(str, self.preorder())) in ''.join(map(str, node.preorder()))
+
+	def contained_in(self, node):
+		"""
+		time complexity: O(n+km), where k is the number of occurences of T2's root in T1
+		>>> n0 = Node(8)
+		>>> n1 = Node(6)
+		>>> n2 = Node(10)
+		>>> n3 = Node(3)
+		>>> n4 = Node(7)
+		>>> n5 = Node(9)
+		>>> n6 = Node(11)
+		>>> n7 = Node(2)
+		>>> n0.set_left(n1)
+		>>> n0.set_right(n2)
+		>>> n1.set_left(n3)
+		>>> n1.set_right(n4)
+		>>> n2.set_left(n5)
+		>>> n2.set_right(n6)
+		>>> n3.set_left(n7)
+		>>> n1.contained_in(n0)
+		True
+		>>> a = Node(1)
+		>>> b = Node(2)
+		>>> c = Node(3)
+		>>> a.set_left(b)
+		>>> a.set_right(c)
+		>>> a.contained_in(n0)
+		False
+		"""
+		if self.match(node):
+			return True
+		else:
+			if node.left is None and node.right is None:
+				return False
+			elif node.left is None:
+				return self.contained_in(node.right)
+			elif node.right is None:
+				return self.contained_in(node.left)
+			else:
+				return self.contained_in(node.left) or self.contained_in(node.right)
+
+
+	def match(self, node):
+		""" if the root matches then continue comparing their offsprings. Once there is a pair of unmatched offsprings,
+		returns False
+		"""
+		# if the value of the two roots are different, there is no need to compare their offsprings. Simply return False
+		if self.value != node.value:
+			return False
+		
+		# both of their children are empty
+		if self.left == node.left == None and self.right == node.right == None:
+			return True
+		elif self.left == node.left == None and self.right != None and node.right != None:
+			return self.right.match(node.right)
+		elif self.right == node.right == None and self.left != None and node.left != None:
+			return self.left.match(node.left)
+		elif self.left != None and self.right != None and node.left != None and node.right != None:
+			return self.left.match(node.left) and self.right.match(node.right)
+
+		# the structure of the two trees are different
+		return False
+
+
+
 
 if __name__ == "__main__":
 	import doctest
