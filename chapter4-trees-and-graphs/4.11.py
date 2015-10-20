8961f37605e266c6daa494f9eb88795e65d31f7b
Yuanxu Li <yuanxu.lee@gmail.com>
2015-10-20 02:03:23 -0400

    binary tree implementation rewritten. separate treenode from tree

--- chapter4-trees-and-graphs/4.11.py
+++ chapter4-trees-and-graphs/4.11.py
@@ -10,81 +10,194 @@ class Node:
 	def __init__(self, value):
 		self.left = None
 		self.right = None
+		self.parent = None
 		self.value = value
-		self.size = None
+		self.size = 1
 
 	def set_left(self, left):
 		self.left = left
+		if left is not None:
+			left.parent = self
 
 	def set_right(self, right):
 		self.right = right
+		if right is not None:
+			right.parent = self
+
+class Tree:
+	def __init__(self):
+		self.root = None
+
+	def _insert(self, value, node):
+		node.size += 1
+		if value < node.value:
+			if node.left is None:
+				node.left = Node(value)
+				node.left.parent = node
+			else:
+				self._insert(value, node.left)
+		else:
+			if node.right is None:
+				node.right = Node(value)
+				node.right.parent = node
+			else:
+				self._insert(value, node.right)
+
+	def insert(self, value):
+		if self.root is None:
+			self.root = Node(value)
+		else:
+			self._insert(value, self.root)
+
+	def _find(self, value, node):
+		if value == node.value:
+			return node
+		elif value < node.value and node.left is not None:
+			self._find(value, node.left)
+		elif value > node.value and node.right is not None:
+			self._find(value, node.right)
+		else:
+			return None
+
+	def find(self, value):
+		if self.root is not None:
+			return self._find(value, self.root)
+		else:
+			return None
+
+	def _delete(self, value, node):
+		node.size -= 1
+		# node in left subtree
+		if node.value < value:
+			if node.left is not None:
+				return self._delete(value, node.left)
+			else:
+				return None
+		# node in right subtree
+		elif node.value > value:
+			if node.right is not None:
+				return self._delete(value, node.right)
+			else:
+				return None
+		# node found
+		else:
+			# if there are no children
+			if node.left is None and node.right is None:
+				if node == node.parent.left:
+					node.parent.set_left(None)
+				else:
+					node.parent.set_right(None)
+			# if there is only a left child
+			elif node.left is not None and node.right is None:
+				if node == node.parent.left:
+					node.parent.set_left(node.left)
+				else:
+					node.parent.set_right(node.left)
+			# if there is only a right child
+			elif node.left is None and node.right is not None:
+				if node == node.parent.left:
+					node.parent.set_left(node.right)
+				else:
+					node.parent.set_right(node.right)
+			# if there are both left and right children
+			else:
+				# find the maximum node in the left subtree, exchange the values between it and the current node,
+				# then delete the maximum node
+				temp_node = node.left
+				while temp_node.right is not None:
+					temp_node.size -= 1
+					temp_node = temp_node.right
+				node.value = temp_node.value
+				if temp_node == temp_node.parent.left:
+					temp_node.parent.left = None
+				else:
+					temp_node.parent.right = None
+
+
+	def delete(self, value):
+		if self.root is None:
+			return None
+
+		if self.root.value == value:
+			if self.root.left is not None and self.root.right is not None:
+				self.root.size -= 1
+				temp_node = self.root.left
+				while temp_node.right is not None:
+					temp_node.size -= 1
+					temp_node = temp_node.right
+				self.root.value = temp_node.value
+				if temp_node == temp_node.parent.left:
+					temp_node.parent.left = None
+				else:
+					temp_node.parent.right = None
+				temp_node.size = 1
+			elif self.root.left is not None and self.root.right is None:
+				self.root.left.parent = None
+				self.root = self.root.left
+			elif self.root.left is None and self.root.right is not None:
+				self.root.right.parent = None
+				self.root = self.root.right
+			else:
+				self.root = None
+		else:
+			self._delete(value, self.root)
+
+
 
 	def get_random_node(self):
 		"""
 		1. get the size of the tree, n
 		2. get a random integer k between [0, n)
 		3. recursively inorder traverse the left subtree or the right subtree according to k
-		>>> n0 = Node(8)
-		>>> n1 = Node(6)
-		>>> n2 = Node(10)
-		>>> n3 = Node(3)
-		>>> n4 = Node(7)
-		>>> n5 = Node(9)
-		>>> n6 = Node(11)
-		>>> n7 = Node(2)
-		>>> n0.set_left(n1)
-		>>> n0.set_right(n2)
-		>>> n1.set_left(n3)
-		>>> n1.set_right(n4)
-		>>> n2.set_left(n5)
-		>>> n2.set_right(n6)
-		>>> n3.set_left(n7)
-		>>> n0.count_size()
+		time complexity: O(logn)
+		>>> T = Tree()
+		>>> T.insert(8)
+		>>> T.insert(6)
+		>>> T.insert(10)
+		>>> T.insert(3)
+		>>> T.insert(7)
+		>>> T.insert(2)
+		>>> T.insert(9)
+		>>> T.insert(11)
+		>>> T.root.size
 		8
-		>>> n0.get_kth_node(n0.left.count_size()).value
+		>>> T.get_kth_node(T.root.left.size).value
 		8
-		>>> n0.get_kth_node(0).value
+		>>> T.get_kth_node(0).value
 		2
-		>>> n0.get_kth_node(1).value
+		>>> T.get_kth_node(1).value
 		3
+		>>> T.delete(8)
+		>>> T.root.size
+		7
+		>>> T.get_kth_node(T.root.left.size).value
+		7
 		"""
-		n = self.count_size()
+		n = self.root.size
 		k = random.randrange(n)
 		return self.get_kth_node(k)
 
 	def get_kth_node(self, k):
+		return self._get_kth_node(k, self.root)
+
+	def _get_kth_node(self, k, node):
 		""" get the kth node (inorder traversal)
 		"""
 		if k < 0:
 			raise Exception("k < 0")
 
-		if self.left is None:
+		if node.left is None:
 			if k == 0:
-				return self
+				return node
 			else:
-				return self.right.get_kth_node(k-1)
+				return self._get_kth_node(k-1, node.right)
 		else:
-			if k < self.left.count_size():
-				return self.left.get_kth_node(k)
-			elif k == self.left.count_size():
-				return self
+			if k < node.left.size:
+				return self._get_kth_node(k, node.left)
+			elif k == node.left.size:
+				return node
 			else:
-				return self.right.get_kth_node(k-self.left.count_size())
-
-	def count_size(self):
-		""" calculate the size of the subtree rooted in this node
-		"""
-		# if the size has been calculated before, return it directly
-		if self.size is None:
-			if self.left is None and self.right is None:
-				self.size = 1
-			elif self.left is None:
-				self.size = 1 + self.right.count_size()
-			elif self.right is None:
-				self.size = 1 + self.left.count_size()
-			else:
-				self.size = 1 + self.left.count_size() + self.right.count_size()
-		return self.size
+				return self._get_kth_node(k-node.left.size, node.right)
 
 if __name__ == "__main__":
 	import doctest
