5772311aca2601ce40be8773531558f0452793fc
Yuanxu Li <yuanxu.lee@gmail.com>
2015-11-18 01:24:06 -0500

    update move robot, to start searching from the destination

--- chapter8-recursion-and-dynamic-programming/8.2.py
+++ chapter8-recursion-and-dynamic-programming/8.2.py
@@ -12,7 +12,7 @@ def move_robot(maze, position=(0, 0)):
 	>>> maze[3][5], maze[4][5], maze[5][5], maze[5][3], maze[5][4], maze[5][3], maze[0][4], maze[1][4] \
 	= False, False, False, False, False, False, False, False
 	>>> move_robot(maze)
-	[(14, 9), (13, 9), (12, 9), (11, 9), (10, 9), (9, 9), (8, 9), (7, 9), (6, 9), (5, 9), (4, 9), (3, 9), (2, 9), (2, 8), (2, 7), (2, 6), (2, 5), (2, 4), (2, 3), (1, 3), (0, 3), (0, 2), (0, 1), (0, 0)]
+	[(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (3, 9), (4, 9), (5, 9), (6, 9), (7, 9), (8, 9), (9, 9), (10, 9), (11, 9), (12, 9), (13, 9), (14, 9)]
 	"""
 	# if the current cell is off limit
 	if position[0] >= len(maze) or position[1] >= len(maze[0]) or maze[position[0]][position[1]] is False:
@@ -34,6 +34,47 @@ def move_robot(maze, position=(0, 0)):
 		# if neither right nor down works out, mark this position as False and return None
 		maze[position[0]][position[1]] = False
 		return None
+
+def move_robot_updated(maze, row=None, col=None, memo=None):
+	""" Instead of searching from the origin, we recursively start searching from the destination. By keeping
+	all visited points in a memo, we do not repeating paths.
+	>>> maze = [[True for i in range(10)] for j in range(15)]
+	>>> maze[3][5], maze[4][5], maze[5][5], maze[5][3], maze[5][4], maze[5][3], maze[0][4], maze[1][4] \
+	= False, False, False, False, False, False, False, False
+	>>> move_robot_updated(maze)
+	"""
+	# alternative for initial call
+	if row is None or col is None or memo is None:
+		row = len(maze) - 1
+		col = len(maze[0]) - 1
+		memo = set()
+
+	# if find the origin, return itself as the path
+	if row == 0 and col == 0:
+		return [(row, col)]
+
+	# if the point has beeen 
+	if (row, col) in memo or not maze[row][col] or row < 0 or col < 0:
+		return None
+
+	# try coming from up
+	path = move_robot_updated(maze, row - 1, col, memo)
+	if path is not None:
+		path.append((row, col))
+		return path
+	# try coming from left
+	path = move_robot_updated(maze, row, col - 1, memo)
+	if path is not None:
+		path.append((row, col))
+		return path
+	# if neither direction works out, mark this position as visited
+	memo.add((row, col))
+	return None
+
+
+
+
+
 
 
 if __name__ == "__main__":
