4837f605ab5fe1ddfcdf4f33158ec799c8983eeb
Yuanxu Li <yuanxu.lee@gmail.com>
2015-11-18 23:05:21 -0500

    update permutations with duplicates. Instead of keeping a memo, I create a frequency table for the string, and pick one character each time during the generation process

--- chapter8-recursion-and-dynamic-programming/8.8.py
+++ chapter8-recursion-and-dynamic-programming/8.8.py
@@ -20,6 +20,38 @@ def permutations_dups(string, prefix=None, memo=None):
 	for i in range(len(string)):
 		yield from permutations_dups(string[:i] + string[i+1:], prefix + string[i], memo)
 
+from collections import defaultdict
+
+def permutations_dups_updated(prefix, remaining=None, freq_table=None):
+	"""
+	We count the frequencies of each character, and then use that to recursively construct permutations.
+	Each time we take a character
+	>>> list(permutations_dups_updated("sst"))
+	['tss', 'sts', 'sst']
+	"""
+	# alternative for initial call
+	if freq_table is None or remaining is None:
+		freq_table = defaultdict(int)
+		for char in prefix:
+			freq_table[char] += 1
+		remaining = len(prefix)
+		yield from permutations_dups_updated("", remaining, freq_table)
+	else:
+		# base case
+		if remaining == 0:
+			yield prefix
+		# recursive case
+		else:
+			for char in freq_table:
+				if freq_table[char] > 0:
+					freq_table[char] -= 1
+					yield from permutations_dups_updated(prefix + char, remaining - 1, freq_table)
+					freq_table[char] += 1
+
+
+
+
+
 if __name__ == "__main__":
 	import doctest
 	doctest.testmod()
