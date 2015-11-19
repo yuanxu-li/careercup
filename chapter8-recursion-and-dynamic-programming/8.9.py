a6f284041f79aebff70e3a758d6d81274e2b8a01
Yuanxu Li <yuanxu.lee@gmail.com>
2015-11-18 23:07:52 -0500

    update parentheses generation method. Instead of keeping a memo, I build a combination from scratch by considering two possibilities for genenration step. 1. add left paren (if there are left parens) 2. add right paren (if the number of left parens is more than that of right parens)

--- chapter8-recursion-and-dynamic-programming/8.9.py
+++ chapter8-recursion-and-dynamic-programming/8.9.py
@@ -7,26 +7,45 @@
 import pdb
 
 def parens(n, memo=None):
-	"""
+	""" We base every case on a previous case. To construct combinations for n pairs from 
 	>>> parens(3)
 	{'(()())', '()(())', '()()()', '((()))', '(())()'}
 	"""
+	# base case
+	if n == 0:
+		return set([""])
+	# recursive case
+	result_set = set()
+	for paren in parens(n-1):
+		result_set.add("()" + paren)
+		result_set.add(paren + "()")
+		result_set.add("(" + paren + ")")
+
+	return result_set
+
+def parens_updated(n, left_rem=None, right_rem=None, prefix=None):
+	""" We build a string from scratch. Each step there are two possibilities:
+	1. add left paren (precondition: still left paren left)
+	2. add right paren (precondition: left paren more than right paren)
+	>>> set(parens_updated(3))
+	{'(()())', '()(())', '()()()', '((()))', '(())()'}
+	"""
 	# alternative for initial call
-	if memo is None:
-		memo = []
-		for i in range(n+1):
-			memo.append(set())
+	if left_rem is None or right_rem is None or prefix is None:
+		left_rem = n
+		right_rem = n
+		prefix = ""
+	# base case
+	if left_rem == 0 and right_rem == 0:
+		yield prefix
+	# recursive case
+	else:
+		if left_rem > 0:
+			yield from parens_updated(n, left_rem - 1, right_rem, prefix + "(")
+		if right_rem > left_rem:
+			yield from parens_updated(n, left_rem, right_rem - 1, prefix + ")")
 
-	for i in range(n+1):
-		if i == 0:
-			memo[i].add("")
-		else:
-			for paren in memo[i-1]:
-				memo[i].add("()" + paren)
-				memo[i].add(paren + "()")
-				memo[i].add("(" + paren + ")")
 
-	return memo[n]
 
 
 
