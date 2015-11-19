fe312d47831d2c16a7bc74e69f72b7637036d9f1
Yuanxu Li <yuanxu.lee@gmail.com>
2015-11-18 23:43:27 -0500

    update paint fill method. just paint the region of the old color using dfs

--- chapter8-recursion-and-dynamic-programming/8.10.py
+++ chapter8-recursion-and-dynamic-programming/8.10.py
@@ -2,29 +2,30 @@
 # That is, given a screen (represented by a two-dimensional array of colors), a point, and a new color,
 # fill in the surrounding area until the color changes from the original color.
 
-def paint_fill(array, point, color, memo=None):
+def paint_fill(array, row, col, new_color, old_color=None):
 	"""
-	>>> paint_fill([[4, 5, 7, 3, 8, 4], [3, 6, 2, 9, 4, 0], [0, 6, 0, 2, 6, 3], [1, 2, 0, 0, 0, 4]], (1, 1), 0)
+	>>> array = [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [1, 1, 1, 1]]
+	>>> paint_fill(array, 1, 3, 9)
 	True
+	>>> array
+	[[9, 9, 9, 9], [9, 9, 9, 9], [1, 1, 1, 1], [1, 1, 1, 1]]
 	"""
 	# alternative for initial call
-	if memo is None:
-		memo = set()
+	if old_color is None:
+		old_color = array[row][col]
 
-	# visited before, return
-	if point in memo:
+	# if the point is off limit
+	if row < 0 or row >= len(array) or col < 0 or col >= len(array[0]):
+		return False
+	# if arrives at the border of old color
+	if array[row][col] != old_color:
 		return True
-	# otherwise, change the color of this point, and recursively change its neighbors
-	array[point[0]][point[1]] = color
-	memo.add(point)
-	if point[0] < len(array) - 1:
-		paint_fill(array, (point[0]+1, point[1]), color, memo)
-	if point[1] < len(array[0]) - 1:
-		paint_fill(array, (point[0], point[1]+1), color, memo)
-	if point[0] > 0:
-		paint_fill(array, (point[0]-1, point[1]), color, memo)
-	if point[1] > 0:
-		paint_fill(array, (point[0], point[1]-1), color, memo)
+	# change the color of this point, and recursively change its neighbors
+	array[row][col] = new_color
+	paint_fill(array, row+1, col, new_color, old_color)
+	paint_fill(array, row, col+1, new_color, old_color)
+	paint_fill(array, row-1, col, new_color, old_color)
+	paint_fill(array, row, col-1, new_color, old_color)
 	return True
 
 if __name__ == "__main__":
