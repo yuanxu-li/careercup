57a63d5bdc14ddbdaf1a13da6ba5a662d0f8e108
Yuanxu Li <yuanxu.lee@gmail.com>
2015-12-22 16:03:12 -0500

    we merge two arrays into the back of the larger array, without any shift

--- chapter10-sorting-and-searching/10.1.py
+++ chapter10-sorting-and-searching/10.1.py
@@ -3,40 +3,37 @@
 
 import pdb
 
-def sort_merge(A, len_A, B, len_B):
+def sort_merge(A, B, len_A):
 	"""
-	move A rightward to leave space for B, and then merge the moved A and the original B into the array A used
-	to occupy
+	If we merge A and B into the back of A, we do not have to shift A.
+
 	>>> A = [1, 3, 5, 6, 7, 8, 9, 11, 0, 0, 0, 0, 0]
 	>>> B = [2, 4, 10]
-	>>> sort_merge(A, 8, B, len(B))
+	>>> sort_merge(A, B, 8)
 	>>> A
 	[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0, 0]
 	"""
-	# move A rightward to leave space for B
-	for i in range(len_A - 1, -1, -1):
-		A[i + len_B] = A[i]
-	# merge A and B into the array A used to occupy
-	i = len_B	# start index of A
-	j = 0 # start index of B
-	k = 0 # start index of the new array
-	while i < len_A + len_B and j < len_B:
+	# merge A and B into the the back of A
+	i = len_A - 1 # backward index of A
+	j = len(B) - 1 # backward index of B
+	k = len_A + len(B) - 1 # backward index of the new array
+	while i >= 0 and j >= 0:
 		if A[i] >= B[j]:
-			A[k] = B[j]
-			k += 1
-			j += 1
-		else:
 			A[k] = A[i]
-			k += 1
-			i += 1
-	while i < len_A + len_B:
+			k -= 1
+			i -= 1
+		else:
+			A[k] = B[j]
+			k -= 1
+			j -= 1
+	while i >= 0:
 		A[k] = A[i]
-		i += 1
-		k += 1
-	while j < len_B:
+		i -= 1
+		k -= 1
+	while j >= 0:
 		A[k] = B[j]
-		j += 1
-		k += 1
+		j -= 1
+		k -= 1
 
 if __name__ == "__main__":
 	import doctest
