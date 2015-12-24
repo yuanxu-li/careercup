8a9d16a3748bf62860b124cad5f4f2ddba71ca77
Yuanxu Li <yuanxu.lee@gmail.com>
2015-12-24 16:08:07 -0500

    firstly find the length in O(logn), then binary search in O(logn)

--- chapter10-sorting-and-searching/10.4.py
+++ chapter10-sorting-and-searching/10.4.py
@@ -5,23 +5,37 @@
 
 def find_index(arr, x):
 	"""
+	The naive approach is O(n) by searching through the entire array.
+	A better approach would be using binary search, but one problem remains:
+	we do not know the array length.
+	Therefore we split this problem into two steps:
+	1. find the length n in O(logn) time, by increasing n from 1, to 2, 4, 8
+	until element_at returns -1 which means it is out of bound
+	2. apply binary search to this array in O(logn)
 	>>> find_index([1,2,5,7,9,10], 9)
 	4
 	>>> find_index([1,2,5,7,9,10], 3)
 	-1
 	"""
-	i = 0
-	while True:
-		e = element_at(arr, i)
-		# find it
-		if e == x:
-			return i
-		# out of bound or the current element is larger than the searched element
-		elif e == -1 or e > x:
-			return -1
-		# continue
+	# find the length
+	n = 1
+	while element_at(arr, n) != -1:
+		n *= 2
+
+	# binary search
+	low = 0
+	high = n - 1
+	while low <= high:
+		mid = (low + high) // 2
+		mid_value = element_at(arr, mid)
+		if mid_value == x:
+			return mid
+		elif mid_value > x or mid_value == -1:
+			high = mid - 1
 		else:
-			i += 1
+			low = mid + 1
+	return -1
+
 
 def element_at(arr, i):
 	if i in range(len(arr)):
