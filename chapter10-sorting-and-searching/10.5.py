0138115e1d88971e767f89ccfc60b43faf436386
Yuanxu Li <yuanxu.lee@gmail.com>
2015-12-27 12:52:40 -0500

    apply binary search and deal with non-emtpy mid element

--- chapter10-sorting-and-searching/10.5.py
+++ chapter10-sorting-and-searching/10.5.py
@@ -1,26 +1,57 @@
 # 10.5 Sparse Search: Given a sorted array of strings that is interspersed with empty strings, write a method to find
 # the location of a given string.
 
-def sparse_search(arr, x):
+def sparse_search(arr, x, first=None, last=None):
 	"""
-	Since the array is interspersed with empty strings, we need another variable to keep track of the indices, called ind.
-	Each time we loop over a non-empty string, we add ind by 1
+	with empty strings interspersed, it is a bit harder to apply binary search,
+	since an empty mid element makes no sense for comparison. However, we can shift
+	the mid index to find the closest mid non-empty element for comparison in order 
+	to decide which half to go to.
 	>>> sparse_search(["aab", "", "", "aabc", "", "abcc", "", "", "abcd", "", "acdf"], "abcd")
-	3
+	8
 	>>> sparse_search(["aab", "", "", "aabc", "", "abcc", "", "", "abcd", "", "acdf"], "abc")
 	-1
 	"""
-	ind = 0
-	for i in range(len(arr)):
-		# find it
-		if arr[i] == x:
-			return ind
-		# the current element is already larger than the searched element
-		elif arr[i] > x:
-			return -1
-		elif arr[i]:
-			ind += 1
-	return -1
+	# alternative for initial call
+	if first is None or last is None:
+		first = 0
+		last = len(arr) - 1
+
+	# edge case
+	if first > last:
+		return -1
+	mid = (first + last) // 2
+
+	# first and last element can be emtpy strings, but gotta make sure
+	# mid element is non-empty for comparison so that we can decide
+	# which half to search
+	if not arr[mid]:
+		left = mid - 1
+		right = mid + 1
+
+		while True:
+			if left < first and right > last:
+				return -1
+			elif arr[left] and first <= left:
+				mid = left
+				break
+			elif arr[right] and right <= last:
+				mid = right
+				break
+			left += 1
+			right += 1
+
+	# now that mid element is non-empty, compare it with searched element x
+	if arr[mid] == x:
+		return mid
+	elif arr[mid] > x:
+		return sparse_search(arr, x, first, mid-1)
+	else:
+		return sparse_search(arr, x, mid+1, last)
+
+
+
+
 
 if __name__ == "__main__":
 	import doctest
