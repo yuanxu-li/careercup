7fbc0fa4961320bc2c38c07e64b970b7e497358c
Yuanxu Li <yuanxu.lee@gmail.com>
2015-12-28 00:46:24 -0500

    update the descriptions of solution without real code

--- chapter10-sorting-and-searching/10.7.py
+++ chapter10-sorting-and-searching/10.7.py
@@ -2,3 +2,7 @@
 # provide an algorithm to generate an integer that is not contained in the file.
 # Assume you have 1 GB of memory available for this task.
 
+"""
+We can map all integers into bits of a series of bytes, and find the first
+non-zero bit along the bytes
+"""
